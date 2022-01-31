557b0ede85c2e14dc5044dcf73b33d3335fbb061
dwwm93 <74361568+dwwm93@users.noreply.github.com>
2022-01-31 17:17:02 +0100

    feat(php): Ajoute l'insertion et le test de l'existence de l'user pour l'inscription

--- 2022-01-25_form_php/page/inscription.php
+++ 2022-01-25_form_php/page/inscription.php
@@ -1,13 +1,54 @@
 <?php
 
 $email = @$_POST['email'];
-$errorMessage = '';
+$password = @$_POST['password'];
+$errorMessage = '<ul>';
 
-if ($email === 'contact@greta.com' || !verifForm('email', $email)) {
+if ($email && !isValid('email', $email)) {
     $color = 'red';
-    $errorMessage = 'email deja existant';
+    $errorMessage .= '<li>email invalide</li>';
 } else {
     $color = 'green';
 }
+
+if ($password && !isValid('password', $password)) {
+    $errorMessage .= '<li>Mot de passe pas assez sécurisé.</li>';
+}
+
+
+try {
+    //code...
+    if (
+        $email
+        && $password
+        && isValid('email', $email)
+        && isValid('password', $password)
+    ) {
+
+        /** @todo à commenter */
+        //require_once('../bdd.php');
+        $exists = $bdd->prepare('SELECT * FROM user
+        WHERE email = ?');
+        $exists->bindParam(1, $email);
+        $exists->execute();
+        $res = $exists->fetchAll(PDO::FETCH_ASSOC);
+        if (count($res) > 0) {
+            $errorMessage .= "user existe";
+        } else {
+            $query = $bdd->prepare("INSERT INTO user (email, password, lastname)
+    VALUE (?, ?, ?)");
+            $inserted = $query->execute([$email, $password, 'TOTO']);
+            if ($inserted) {
+                $errorMessage .= "User bien inséré.";
+            } else {
+                $errorMessage .= "Erreur durant l'insertion.";
+            }
+        }
+    }
+} catch (\Throwable $th) {
+    echo ($th->getMessage());
+}
+
+$errorMessage .= "</ul>";
 
 include_once('template/inscriptionForm.php');
