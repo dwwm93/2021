4afb34d7a1e45aebac82b7722979a030dc1415bd
dwwm93 <74361568+dwwm93@users.noreply.github.com>
2022-02-01 17:07:28 +0100

    feat(php): Footer groot

--- 2022-01-25_form_php/template/footer.php
+++ 2022-01-25_form_php/template/footer.php
@@ -1,3 +1,8 @@
+</main>
+<footer>
+    <p>I AM GROOT</p>
+    <img style="width: 80px" src="https://www.pngarts.com/files/4/Baby-Groot-Free-PNG-Image.png" alt="" srcset="" />
+</footer>
 </body>
 
 </html>
