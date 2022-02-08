3f6cd5818644a268ac2c75e23c0dae8a9d16c6a5
marrco <marrco@wohecha.fr>
2022-02-08 16:04:06 +0100

    feat(php): Affiche l'auteur de la news

--- 2022-02-07_news/page/article.php
+++ 2022-02-07_news/page/article.php
@@ -2,7 +2,12 @@
 
 $id = $_GET['id'];
 /** @var PDO $bdd */
-$query = $bdd->prepare("SELECT * FROM article
+$query = $bdd->prepare("SELECT 
+    article.*, 
+    user.nom_user, 
+    user.prenom_user 
+FROM article
+JOIN user ON article.id_user = user.id_user
 WHERE id_article = ?");
 $query->bindParam(1, $id);
 $query->execute();
--- 2022-02-07_news/template/articles_show.php
+++ 2022-02-07_news/template/articles_show.php
@@ -1,4 +1,9 @@
 <h2><?php echo ($article['titre_article']); ?></h2>
+<summary>par
+    <a href="index.php?page=users&id=<?= $article['id_user'] ?>">
+        <?= "{$article['nom_user']} {$article['prenom_user']}" ?>
+    </a>
+</summary>
 
 <img src="<?= $article['img_article'] ?>" alt="" srcset="">
 <p>
