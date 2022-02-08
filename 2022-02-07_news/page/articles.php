06659fc9ec7496bf31630a612bf863fdbf477d56
marrco <marrco@wohecha.fr>
2022-02-08 16:04:34 +0100

    feat(php): Affiche les catégories des articles

--- 2022-02-07_news/page/articles.php
+++ 2022-02-07_news/page/articles.php
@@ -1,7 +1,7 @@
 <?php
 
 /** @var PDO $bdd */
-$query = $bdd->prepare("SELECT * FROM article");
+$query = $bdd->prepare("SELECT article.id_article, article.titre_article, categorie.nom_categorie FROM article JOIN categorie ON article.id_categorie = categorie.id_categorie;");
 $query->execute();
 $articles = $query->fetchAll();
 
--- 2022-02-07_news/template/articles_list.php
+++ 2022-02-07_news/template/articles_list.php
@@ -2,9 +2,10 @@
 
 <ol>
     <?php
-    foreach ($articles as $key => $article) {
-        echo ("<li><a href='index.php?page=articles&id={$article['id_article']}'>
-                {$article['titre_article']}</a>
+    foreach ($articles as $key => $colonnes) {
+        echo ("<li><a href='index.php?page=articles&id={$colonnes['id_article']}'>
+                [{$colonnes['nom_categorie']}] - 
+                {$colonnes['titre_article']}</a>
             </li>");
     }
     ?>
