77140359b5ccbb4d27574cf80de2e771a8ae064b
dwwm93 <74361568+dwwm93@users.noreply.github.com>
2022-02-15 14:18:20 +0100

    feat(symfony): Initialise site news

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
         <li><a href="/2022-01-17_php/">2022-01-17_php</a></li>
         <li><a href="/2022-01-25_form_php/">2022-01-25_form_php</a></li>
         <li><a href="/2022-02-07_news/">2022-02-07_news</a></li>
+        <li><a href="/2022-02-15_symfony/">2022-02-15_symfony</a></li>
     </ul>
 </body>
 
