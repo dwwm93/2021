c7823e16f3fd8c721e020d4f75e8eb60196dcaf2
dwwm93 <74361568+dwwm93@users.noreply.github.com>
2022-02-28 17:00:43 +0100

    feat(symfony): ORM (Entity, Repository)
    todo : manager

--- 2022-02-15_symfony/src/Controller/ArticleController.php
+++ 2022-02-15_symfony/src/Controller/ArticleController.php
@@ -2,11 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ArticleController extends AbstractController
 {
+
+    /**
+     * @Route("/articles/{id}", name="article")
+     */
+    public function show($id): Response
+    {
+        $article = $this
+            ->getDoctrine()
+            ->getRepository(Article::class)
+            ->find($id);
+
+        return $this->render('article/show.html.twig', [
+            'article' => $article,
+        ]);
+    }
 
     /**
      * @Route("/articles", name="articles")
@@ -15,14 +32,18 @@ class ArticleController extends AbstractController
     {
         /**
          * Traitements intelligents
+         * @var Article[] $articles
          */
-
+        $articles = $this
+            ->getDoctrine()
+            ->getRepository(Article::class)
+            ->findAll();
 
         /**
          * Renvoie la vue !
          */
         return $this->render('article/article_list.html.twig', [
-            'test' => "hola que tal !",
+            'articles' => $articles,
         ]);
     }
 }
