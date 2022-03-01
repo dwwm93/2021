de3c9be535289c0c98251148b991f9cc33e1c33b
dwwm93 <74361568+dwwm93@users.noreply.github.com>
2022-03-01 13:28:25 +0100

    cours(symfony): Injection de dépendances

--- 2022-02-15_symfony/src/Controller/ArticleController.php
+++ 2022-02-15_symfony/src/Controller/ArticleController.php
@@ -3,15 +3,43 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Form\ArticleType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class ArticleController extends AbstractController
 {
+    /**
+     * @Route("/articles/new", name="create_article")
+     */
+    function create(Request $request): Response
+    {
+        // Si "$_POST" n'est pas rempli :
+        $form = $this->createForm(ArticleType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Si $_POST est rempli
+            // 1. lire les données du POST
+            // 2. créer une nouvelle entité $article, 
+            //    la remplir avec les données
+            // 3. sauvegarde
+            // 4 a. je l'affiche dans le formulaire
+            // 4 b. je redirige vers l'article créé
+            return $this->redirectToRoute("accueil");
+        }
+
+        $viewForm = $form->createView();
+
+        return $this->render('article/create.html.twig', [
+            'viewForm' => $viewForm,
+        ]);
+    }
 
     /**
-     * @Route("/articles/{id}", name="article")
+     * @Route("/articles/{id}", name="show_article")
      */
     public function show($id): Response
     {
