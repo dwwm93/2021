ae80a83757538651edd02a1612c308a61d0abed3
dwwm93 <74361568+dwwm93@users.noreply.github.com>
2022-03-01 17:13:18 +0100

    cours(symfony): ParamConverter

--- 2022-02-15_symfony/src/Controller/ArticleController.php
+++ 2022-02-15_symfony/src/Controller/ArticleController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Form\ArticleType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,19 +14,31 @@ class ArticleController extends AbstractController
 {
     /**
      * @Route("/articles/new", name="create_article")
+     * @Route("/articles/{id}/update", name="update_article")
      */
-    function create(Request $request): Response
-    {
+    function create(
+        Request $request,
+        EntityManagerInterface $manager,
+        Article $article = null
+    ): Response {
+        if (!$article) {
+            $article = new Article();
+        }
         // Si "$_POST" n'est pas rempli :
-        $form = $this->createForm(ArticleType::class);
+        $form = $this->createForm(ArticleType::class, $article);
 
         $form->handleRequest($request);
+
         if ($form->isSubmitted() && $form->isValid()) {
             // Si $_POST est rempli
             // 1. lire les données du POST
             // 2. créer une nouvelle entité $article, 
             //    la remplir avec les données
+
             // 3. sauvegarde
+            $manager->persist($article);
+            $manager->flush();
+
             // 4 a. je l'affiche dans le formulaire
             // 4 b. je redirige vers l'article créé
             return $this->redirectToRoute("accueil");
@@ -41,13 +54,14 @@ class ArticleController extends AbstractController
     /**
      * @Route("/articles/{id}", name="show_article")
      */
-    public function show($id): Response
+    public function show(Article $article): Response
+    //    public function show($id): Response
     {
-        $article = $this
+        /* $article = $this
             ->getDoctrine()
             ->getRepository(Article::class)
             ->find($id);
-
+ */
         return $this->render('article/show.html.twig', [
             'article' => $article,
         ]);
